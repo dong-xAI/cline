d1751aaf86fbbb1f7b184c6ad8c744b08e13ae6c
yleo77 <ylep77@gmail.com>
2025-05-22 23:49:58 +0800

    feat: add default Japi MCP config

--- src/services/mcp/defaultMcpServers.ts
+++ src/services/mcp/defaultMcpServers.ts
@@ -3,6 +3,10 @@ export const DEFAULT_MCP_SERVERS = {
 		command: "npx",
 		args: ["-y", "figma-developer-mcp", "--figma-api-key=YOUR-KEY", "--stdio"],
 	},
+	"Framelink Japi MCP": {
+		command: "npx",
+		args: ["--registry=http://registry.m.jd.com", "@jd/japi-mcp-server"],
+	},
 } as const
 
 export type DefaultMcpServers = typeof DEFAULT_MCP_SERVERS
